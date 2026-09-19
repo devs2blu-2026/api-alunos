abd8b797879155c760889ae91dd82df8df15ff8d
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-09-18 21:43:15 -0300

    Adiciona response entity

--- src/main/java/com/delegrego/api_alunos/controller/AlunoController.java
+++ src/main/java/com/delegrego/api_alunos/controller/AlunoController.java
@@ -2,6 +2,10 @@ package com.delegrego.api_alunos.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,23 +31,30 @@ public class AlunoController {
 	}
 
 	@GetMapping
-	public List<AlunoResponse> listarAlunos() {
-		return service.listarAlunos();
+	public ResponseEntity<List<AlunoResponse>> listarAlunos() {
+		return ResponseEntity.status(HttpStatus.OK).body(service.listarAlunos());
 	}
 
 	@GetMapping("/{id}")
-	public AlunoResponse obterAlunoPorId(@PathVariable int id) {
-		return service.obterAlunoPorId(id);
+	public ResponseEntity<AlunoResponse> obterAlunoPorId(@PathVariable int id) {
+		return ResponseEntity.status(HttpStatus.OK).body(service.obterAlunoPorId(id));
 	}
 
 	@PostMapping
-	public AlunoResponse cadastrarAluno(@Valid @RequestBody AlunoRequest request) {
-		return service.cadastrarAluno(request);
+	public ResponseEntity<AlunoResponse> cadastrarAluno(@Valid @RequestBody AlunoRequest request) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.cadastrarAluno(request));
 	}
 
 	@PutMapping("/{id}")
-	public AlunoResponse atualizarAluno(@PathVariable int id, @Valid @RequestBody AlunoRequest request) {
-		return service.atualizarAluno(id, request);
+	public ResponseEntity<AlunoResponse> atualizarAluno(@PathVariable int id,
+			@Valid @RequestBody AlunoRequest request) {
+		return ResponseEntity.status(HttpStatus.OK).body(service.atualizarAluno(id, request));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> excluirAluno(@PathVariable int id) {
+		service.excluirAluno(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
 }
